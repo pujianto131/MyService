2dc6574d2e876a1ed1278c6b254d9c23ec82a6a7
pujianto131 <pujianto.e131@gmail.com>
2018-07-29 18:40:29 +0700

    first upload

---
+++ app/src/main/java/com/pujianto131/myservice/OriginService.java
@@ -0,0 +1,54 @@
+package com.pujianto131.myservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.IBinder;
+import android.util.Log;
+
+public class OriginService extends Service {
+    public static final String ORIGIN_SERVICE = "OriginService";
+
+    public OriginService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flage, int startId){
+        Log.d(ORIGIN_SERVICE, "origin Service dijalankan");
+        ProcessAsync mProcessAsync = new ProcessAsync();
+        mProcessAsync.execute();
+        return START_STICKY;
+    }
+    private class ProcessAsync extends AsyncTask<Void, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                Thread.sleep(3000)  ;
+            }
+            catch (InterruptedException e){
+
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid){
+            super.onPostExecute(aVoid);
+            Log.d(ORIGIN_SERVICE, "Stop Service");
+            stopSelf();
+        }
+
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(ORIGIN_SERVICE, "onDestroy");
+    }
+
+}
